63a1156f9badc32b5edbd8fcb758b5e9b9ecf0c3
akhil kumar <52838239+Akhil-64@users.noreply.github.com>
2019-11-07 23:02:28 +0530

    Add files via upload

---
+++ button.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+m=tk.Tk()
+m.title("button")
+button=tk.Button(m,width=25,height=10,font=25,text="button",activeforeground="red",activebackground="powder blue",bg="pink")
+button.pack()
+m.mainloop()
